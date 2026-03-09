5b419b4d352c3be6dceb98754d38b808f72304fe
KunbaoG8168 <3B017096@gm.student.ncut.edu.tw>
2026-03-09 09:10:59 +0800

    update main

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@
 4.提供基本 API 路由
 5.作為 uvicorn 啟動的入口
 '''
+'''
 #用來建立 API 伺服器
 from fastapi import FastAPI
 #匯入在database.py裡定義的 engine、Base
@@ -15,12 +16,27 @@ from app.database import engine, Base
 #讓 Python 讀取這兩個 model 檔案
 #如果沒 import，create_all() 會找不到表，什麼都不會建立。
 from app.models import case, document
+from app.routers import case
+
+'''
+from fastapi import FastAPI
+from app.database import engine, Base
+
+# models 只負責建表，不要佔用 case 名稱
+from app.models import case as case_model
+from app.models import document as document_model
+
+# router 用另一個名字
+from app.routers import case as case_router
+
 
 #http://127.0.0.1:8000/docs
 app = FastAPI(title="Patent Docketing System")
 
 #根據所有繼承 Base 的 class，自動建立資料表
 Base.metadata.create_all(bind=engine)
+
+app.include_router(case_router.router)
 
 @app.get("/")
 def root():
